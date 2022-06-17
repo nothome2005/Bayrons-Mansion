206cd1af866eebfa0dbdc2c749670e0934517b7d
nothome2005 <67219161+nothome2005@users.noreply.github.com>
2022-06-17 23:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask, request
 import pymongo
+from time import sleep
+import requests
+from fake_useragent import UserAgent
+from threading import Thread
 
+ua = UserAgent()
 client = pymongo.MongoClient("mongodb+srv://Fit4a:S8Lqaagemi98rTt@cluster0.dzzqz.mongodb.net/Godot?retryWrites=true&w=majority")
 db = client.Godot
 coll = db.Secrets_of_Bayron_Mansion
+
+
+def ping_timer():
+    while True:
+        requests.get('https://bayronsmansion.herokuapp.com/user?nickname=super4', headers={'User-Agent': ua.random})
+        print('Herok wake up!')
+        sleep((60*15))
+    print('Heroku sleep!')
+    
+th = Thread(target=ping_timer)
+th.start()
+
 
 # create the Flask app
 app = Flask(__name__)
